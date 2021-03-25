8cf86232995c0917acd26109ea0b4695c8d47605
Elson Júnior <elson.araujo.bw@gmail.com>
2021-03-24 22:41:18 -0400

    [C01P05] setup app config overnight and express

---
+++ test/globals.d.ts
@@ -0,0 +1,7 @@
+declare namespace NodeJS {
+	interface Global {
+		// Typesetting that already exists
+		// Import inline, able to overwrite/add global types
+		testRequest: import('supertest').SuperTest<import('supertest').Test>;
+	}
+}
